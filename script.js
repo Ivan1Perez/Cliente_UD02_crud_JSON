fd316a11e6e59ab1d808c46ff5937989ca97b506
Ivan <ivan_psh@hotmail.com>
2023-10-03 09:08:29 +0200

    Funcion "searchProdById"

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+/*
+Create a program to search for products with the following characteristics:
+• It should accept the following product features (Id, Name, Description, Category, 
+Price (euros), Rating (1-5)).COMPLETED
+• We should be able to search, add, modify, and delete products.
+• We should be able to search for products by price (higher to lower or lower to 
+higher).
+• We should be able to search for products in alphabetical order (a-z and z-a).
+• We should be able to search by rating (higher to lower and lower to higher).
+• We should be able to filter by category and then order by price, alphabetical 
+order or rating.
+• The products will be stored in a JSON structure. When you perform a filtering, the 
+result will be stored in a JSON structure. (extra credit
+*/
+
 class Product {
   id;
   name;
@@ -22,7 +37,7 @@ class Product {
 
 let products = [];
 products[0] = new Product(
-  748923,
+  0,
   "Intel Core i7-13700KFtel",
   "Amazing 3.6 GHz Processor",
   "Processors",
@@ -30,7 +45,7 @@ products[0] = new Product(
   4.8
 );
 products[1] = new Product(
-  446588,
+  1,
   "Asus ROG Strix B550-F",
   "Super gaming motherboard",
   "Motherboard",
@@ -38,7 +53,7 @@ products[1] = new Product(
   4.6
 );
 products[2] = new Product(
-  156718,
+  2,
   "ZOTAC ZT-A30500E-10M",
   "Powerful gaming graphics card",
   "Graphics card",
@@ -54,47 +69,53 @@ let genericErrorMsg =
   "Error. The character introduced is not valid. Please. Try again.\n";
 let crudOptionsCompleteMsg = new Array(selectOptionsMsg, crudOptionsMsg);
 let answerIsValid = true;
+let searchProdAnswer;
+let searchProdMsg = `Introduce de Id number of the product you want to search.`;
+let noNumberError = `Error. The character introduced is not a number. Please, try again.\n`;
+let product;
 
 //Functions:
 
 function main() {
-  crudOptionSelected = optionsFunct(
-    crudOptionsCompleteMsg,
-    genericErrorMsg,
-    answerIsValid
-  );
+  crudOptionSelected = prompt(`${selectOptionsMsg}${crudOptionsMsg}`);
 
-  do {
-    if (
-      crudOptionSelected !== "1" &&
-      crudOptionSelected !== "2" &&
-      crudOptionSelected !== "3" &&
-      crudOptionSelected !== "4"
-    ) {
-      answerIsValid = false;
-      crudOptionSelected = optionsFunct(
-          crudOptionsCompleteMsg,
-          genericErrorMsg,
-          answerIsValid
-      );
-    } else {
-      answerIsValid = true;
-    }
-  } while (!answerIsValid);
+  while (
+    crudOptionSelected !== "1" &&
+    crudOptionSelected !== "2" &&
+    crudOptionSelected !== "3" &&
+    crudOptionSelected !== "4"
+  ) {
+    crudOptionSelected = prompt(`${genericErrorMsg}${selectOptionsMsg}${crudOptionsMsg}`);
+  }
+
+  switch (crudOptionSelected) {
+    case "1":
+      searchProdAnswer = prompt(searchProdMsg);
+      while (isNaN(searchProdAnswer.trim()) || searchProdAnswer.trim() === "") {
+        searchProdAnswer = prompt(`${noNumberError}${searchProdMsg}`);
+      }
+      product = products.find(searchProdById);
+      break;
+
+    case "2":
+      break;
+
+    case "3":
+      break;
+
+    case "4":
+      break;
+
+    default:
+      break;
+  }
+
+console.log(product);
+
 }
 
-function optionsFunct(messages, errorMsg, isValid) {
-  let completeMsg = "";
-
-  for (const message of messages) {
-    completeMsg += message;
-  }
-
-  if (!isValid) {
-    completeMsg = errorMsg + completeMsg;
-  }
-
-  return prompt(`${completeMsg}`);
+function searchProdById(product) {
+  return product.id === 2;
 }
 
 main();
